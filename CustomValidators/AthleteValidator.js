b470447bd033c93900bdaefd899b5f0e81d9571c
naachovega <igna.vega@hotmail.com>
2022-11-04 20:55:16 -0300

    Se agregan validaciones nuevas para el atleta

---
+++ CustomValidators/AthleteValidator.js
@@ -0,0 +1,21 @@
+import { AthleteRepository } from '../Repository/athlete_repository.js'
+
+const repositorio = new AthleteRepository()
+export const validateUser = dni => {
+
+    return repositorio.buscarUnAtleta(dni).then(usuario => {
+        if (usuario.length > 0) {
+            return Promise.reject(`El usuario ${usuario[0].nombre} ${usuario[0].apellido} ya existe`)
+        }
+    })
+}
+
+export const userExists = dni => {
+
+    return repositorio.buscarUnAtleta(dni).then(usuario =>{
+        if (usuario.length === 0) {
+            return Promise.reject(`El usuario enviado no existe`)
+        }
+    })
+
+}
