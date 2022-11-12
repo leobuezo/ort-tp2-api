56e417e507f71e2f5b8b5b0daa9d9f8e02e0ba79
naachovega <igna.vega@hotmail.com>
2022-11-12 02:05:26 -0300

    Se corrigio el orden de los import en App.js

--- app.js
+++ app.js
@@ -1,26 +1,22 @@
 import express from 'express';
-import path from 'path';
+
 //swagger
 import {swaggerDocs} from './config/swagger.js'
-
-
-
-const app = express()
-const port = 3000
 import atheles from "./routes/athletes.js"
 import coaches from "./routes/coaches.js"
 import admin from "./routes/admin.js"
 import session from "./routes/session.js"
 import router from './routes/class.js'
 import feedback from './routes/feedback.js'
+import { port } from './config.js';
+
+const app = express()
 
 
 //middleware
 app.use(express.json())
 
-
-
-
+//routes
 app.use("/athletes", atheles)
 app.use("/coaches", coaches)
 app.use("/admin", admin)
@@ -28,7 +24,6 @@ app.use("/session", session)
 app.use("/class", router)
 app.use("/feedback", feedback)
 
-//routes
 app.get("/" , (req,res) => {
     res.json({
         status: 'API currently running'
